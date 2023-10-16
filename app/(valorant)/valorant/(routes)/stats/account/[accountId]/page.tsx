dd72897248d4848b4bfe9e8807e78f7ce58f0a82
GavinRobson <gtrobson3@gmail.com>
2023-10-15 23:03:16 -0400

    new commit

--- app/(valorant)/valorant/(routes)/stats/account/[accountId]/page.tsx
+++ app/(valorant)/valorant/(routes)/stats/account/[accountId]/page.tsx
@@ -2,6 +2,7 @@ import { ValorantStatsModal } from "@/components/modals/valorant-stats";
 import { currentValorant } from "@/lib/current-valorant";
 import { redirectToSignIn } from "@clerk/nextjs";
 
+
 const AccountStatsPage = async () => {
   const valorant = await currentValorant();
 
